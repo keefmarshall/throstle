2f7c8df51dd45118e8b55c1f56dd59d4c34df72c
Keith Marshall <code@eleusis.co.uk>
2016-12-17 00:11:17 +0000

    Catch non-numeric lengths. Respond appropriately.

--- src/WebController.kt
+++ src/WebController.kt
@@ -19,12 +19,23 @@ class WebController(dictdir: String)
             response.send(passRes.password, "text/plain")
     }
 
+    val insults = listOf( "Silly sausage", "Don't give up the day job", "You muppet", "How rude",
+            "Your mother was a hamster", "Your father smelt of elderberries",
+            "You empty-headed animal food trough wiper")
+
     val fixedLengthPassword = routeHandler {
         val lengthParam = request.routeParams["length"]
-        val length = lengthParam?.toInt() ?: 0
-        if (length < 8 || length > 40)
+        val length = try { lengthParam?.toInt() ?: 0 } catch(e: NumberFormatException) { -1 }
+
+        if (length <= 0)
         {
-            response.setStatus(400, "Invalid length")
+            response.setStatus(400, "Bad Request")
+            val insult = insults[random.nextInt(insults.size)]
+            response.send("$insult, that is not a helpful password length. Try again.", "text/plain")
+        }
+        else if (length < 8 || length > 40)
+        {
+            response.setStatus(400, "Bad Request")
             response.send("Length must be between 8 and 40", "text/plain")
         }
         else
