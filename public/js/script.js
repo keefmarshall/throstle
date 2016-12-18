cd4ee186b6e4b04a788380f63f9709b63296963d
Keith Marshall <code@eleusis.co.uk>
2016-12-18 18:36:43 +0000

    Loading spinner and visual error handling

--- public/js/script.js
+++ public/js/script.js
@@ -7,11 +7,20 @@ $(document).ready(function() {
 });
 
 function populatePasswordAndTerms(length) {
+    $("#password").html("<span class='glyphicon glyphicon-repeat glyphicon-spin'></span> Loading...");
+
     var url = (length != null) ? "/password/" + length + "?full" : "/password?full";
+
     $.getJSON(url, function(passRes) {
         $("#password").text(passRes.password);
         $("#term1").text(passRes.term1);
         $("#term2").text(passRes.term2);
+    }).error(function() {
+         $("#password").html(
+            "<span class='text-danger'><span class='glyphicon glyphicon-warning-sign'></span>" +
+            " An error occurred, please try again later</span>");
+         $("#term1").text("");
+         $("#term2").text("");
     });
 
     return false;
